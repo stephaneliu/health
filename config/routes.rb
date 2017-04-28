4da09159702ab52374278572d90c5a110d678b97
Stephane Liu <sliu@sjliu.com>
2017-04-28 11:19:02 -1000

    Move mount to top of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  mount_griddler # defaults path /email_processor
+
   root 'home#index'
   devise_for :users
-  mount_griddler # defaults path /email_processor
 end
