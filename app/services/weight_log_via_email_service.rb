a4020272205f5acadad874255324cffeebae4eda
Stephane Liu <sliu@sjliu.com>
2017-04-28 10:16:39 -1000

    Remove create record. Config on mailcloudin

--- app/services/weight_log_via_email_service.rb
+++ app/services/weight_log_via_email_service.rb
@@ -6,7 +6,6 @@ class WeightLogViaEmailService
   end
 
   def process
-    User.create!(email: "some_#{rand(1000)}@test.com", password: "#{rand(100)}_he_#{rand(100)}_there")
     Rails.logger.debug "## Email from: #{email.from[:email]}"
     Rails.logger.debug "## Email subject: #{email.subject}"
     Rails.logger.debug "## Email body: #{email.body}"
