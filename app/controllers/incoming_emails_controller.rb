6e13bba9ab7e27c5552521476d0fd6209a91906d
Stephane Liu <sliu@sjliu.com>
2017-05-10 21:57:32 -1000

    Fix deprecation method

--- app/controllers/incoming_emails_controller.rb
+++ app/controllers/incoming_emails_controller.rb
@@ -1,8 +1,7 @@
 class IncomingEmailsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   def create
-
     Rails.logger.debug "### body content: #{params[:plain]}"
     Rails.logger.debug "### subject: #{params[:subject]}"
     Rails.logger.debug "### headers: #{params[:headers]}"
